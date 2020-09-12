dd1b051f600db1c9a73514d3f3e6e0de481a5da7
rgabelarde <rgabelarde@gmail.com>
2020-09-12 21:44:54 +0800

    Arbitrary commit for team PR

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -21,5 +21,6 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(MainApp.class, args);
+        System.out.println("Arbitrary Commit");
     }
 }
