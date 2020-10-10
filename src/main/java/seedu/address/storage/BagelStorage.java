c0acfba3e787e77b4c515f51aa8a907badf1a58a
Sushi <54584459+yuki-cell@users.noreply.github.com>
2020-10-10 15:37:14 +0800

    Rename Person and AddressBook in Storage

--- src/main/java/seedu/address/storage/BagelStorage.java
+++ src/main/java/seedu/address/storage/BagelStorage.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyBagel;
-
 /**
  * Represents a storage for {@link seedu.address.model.Bagel}.
  */
