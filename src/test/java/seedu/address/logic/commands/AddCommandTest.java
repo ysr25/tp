dd2d1972c44491327aa3cd813dd62c1a20be29a7
Yuxi <liuyuxi.yx@gmail.com>
2020-10-21 22:37:05 +0800

    Edit checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -9,6 +9,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
@@ -153,6 +154,12 @@ public class AddCommandTest {
         public void updateFilteredFlashcardList(Predicate<Flashcard> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void sortFlashcardList(Comparator<Flashcard> comparator) {
+            throw new AssertionError("This method should not be called.");
+        }
+
     }
 
     /**
