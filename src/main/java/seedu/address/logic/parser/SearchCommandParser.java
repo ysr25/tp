19b90866c9c24976c40166a8bc20f1a90ee09ed3
ysr <yongsr12345@gmail.com>
2020-10-21 16:03:08 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/SearchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchCommandParser.java
@@ -3,15 +3,10 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_KEYWORD;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.AddCommand;
+import java.util.stream.Stream;
+
 import seedu.address.logic.commands.SearchCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.flashcard.Description;
-import seedu.address.model.flashcard.Flashcard;
-import seedu.address.model.flashcard.Title;
-
-import java.util.stream.Stream;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -19,8 +14,17 @@ import java.util.stream.Stream;
 public class SearchCommandParser implements Parser<SearchCommand> {
 
     /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public SearchCommand parse(String args) throws ParseException {
@@ -35,13 +39,5 @@ public class SearchCommandParser implements Parser<SearchCommand> {
         String keyword = argMultimap.getValue(PREFIX_KEYWORD).get();
 
         return new SearchCommand(keyword);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
